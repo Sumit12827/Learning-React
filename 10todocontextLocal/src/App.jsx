f8b48c00f80e01653aca514cc9535d146387d45f
Sumit12827 <sumitdowney@gmail.com>
2026-03-06 16:49:09 +0530

    definetheFunction

--- 10todocontextLocal/src/App.jsx
+++ 10todocontextLocal/src/App.jsx
@@ -7,7 +7,13 @@ import { TodoProvider } from './contexts'
 function App() {
   const [todos, setTodos] = useState([])
 
+  const addTodo = (todo) => {
+    setTodos((prev) => [{id: Date.now() , ...todo} , ...prev])
+  }
 
+  const updateTodo = (id , todo) => {
+    setTodos((prev) => prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo)))
+  }
 
 
   return (
